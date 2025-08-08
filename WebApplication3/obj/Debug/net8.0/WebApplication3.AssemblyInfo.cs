65fb9a47c08809579e855070e12c1b43fd7c1aaf
TomGijsbers <r0878924@student.thomasmore.be>
2025-08-08 18:44:07 +0200

    extra endpoint

--- WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
+++ WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a12627819f9168eb0dbd0fe8113f1645e4a3102")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903a0ac072cbd26bb50389864348a3047844ddf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
